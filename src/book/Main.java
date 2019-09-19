02217a5897054b35880c0d822d4b9abddbeb4ef2
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2019-09-19 19:18:43 +0530

    create a main class with main method containing input statements and  program build up

---
+++ src/book/Main.java
@@ -0,0 +1,25 @@
+package book;
+
+//import java.awt.print.Book;
+import java.util.Scanner;
+import book.Book;
+
+    public class Main {
+        public static void main(String[] args) {
+            final Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            sc.nextLine().trim();
+            Book[] books = new Book[n];
+            for (int i = 0; i < n; i++) {
+                String bname = sc.nextLine().trim();
+                String baname = sc.nextLine().trim();
+                String isbn = sc.nextLine().trim();
+                books[i] = new Book(bname, baname, isbn);
+            }
+            for (int i = 0; i < books.length; i++) {
+                System.out.println(books[i]);
+            }
+            sc.close();
+        }
+    }
+
