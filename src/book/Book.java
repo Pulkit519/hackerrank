506a393e98049391c965d127091cf3208cbf9d6e
Pulkit519 <pulkit.gla_cs18@gla.ac.in>
2019-09-19 19:07:40 +0530

    added get method for getting values

--- src/book/Book.java
+++ src/book/Book.java
@@ -3,5 +3,15 @@ package book;
 public class Book {
     private String bname;
     private String isbn;
-    private String authorname;
+    private String baname;
+
+    public String getBname(){
+        return bname;
+    }
+    public String getBaname(){
+        return baname;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
     }
